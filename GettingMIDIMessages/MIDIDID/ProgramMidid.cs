3f66d26648e273bb4930d340909a1f6c3e8d1c60
Nazar_ius <lordusen@gmail.com>
2021-12-25 16:52:08 +0200

    commit for task 6, all info is in the README file

---
+++ GettingMIDIMessages/MIDIDID/ProgramMidid.cs
@@ -0,0 +1,28 @@
+﻿using Melanchall.DryWetMidi.Multimedia;
+using System;
+
+namespace MIDIDID
+{
+    public class ProgramMidid
+    {
+        private static IInputDevice _inputDevice;
+
+        public string noteOnHold { get; set; }
+
+        public void StartListeting()
+        {
+            _inputDevice = InputDevice.GetByIndex(0);
+            _inputDevice.EventReceived += OnEventReceived;
+            _inputDevice.StartEventsListening();
+            
+            (_inputDevice as IDisposable)?.Dispose();
+        }
+
+        public void OnEventReceived(object sender, MidiEventReceivedEventArgs e)
+        {
+            var midiDevice = (MidiDevice)sender;
+            noteOnHold = e.Event.ToString();
+            //Console.WriteLine($"Event received from '{midiDevice.Name}' at {DateTime.Now}: {e.Event}");
+        }
+    }
+}
---
+++ GettingMIDIMessages/MIDIhelder/Form1.cs
@@ -0,0 +1,50 @@
+﻿using MIDIDID;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIDIhelder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        static string pip;
+        public string ChangeTextBoxViaMidiDevice()
+        {
+            var midi = new ProgramMidid();
+            string parseME = midi.noteOnHold;
+            return parseME;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var midi = new ProgramMidid();
+            midi.StartListeting();
+            pip = midi.noteOnHold;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                if (pip.StartsWith("Note On [0] (36"))
+                {
+                    label1.Text = "note c";
+                }
+                if (pip.StartsWith("Note On [0] (37"))
+                {
+                    label1.Text = "note c#";
+                }
+            }
+        }
+    }
+}
